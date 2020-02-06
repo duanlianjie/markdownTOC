45cf0ef47719e9e1b502b87ce6e276f6ab22092e
duanlianjie <1796248113@qq.com>
2020-02-06 23:18:15 +0800

    修复标题级别Bug，完善脚本在命令行运行时的参数设置

--- README.md
+++ README.md
@@ -1,27 +1,56 @@
 - [markdownTOC](#markdownTOC)
 
+	- [环境](#环境)
+
 	- [运行](#运行)
 
-	- [特别情况考虑](#特别情况考虑)
+	- [特殊情况考虑](#特殊情况考虑)
 
 # markdownTOC
 
 python脚本，用于生成markdown文件的TableOfContent，解决Github Flavored Markdown不支持[TOC]显示的问题
 
+## 环境
+
+```
+python 3.6
+chardet 3.0.4
+```
+
 ## 运行
 
-打开脚本所在目录的命令行，运行以下命令即可，生成新的markdown将覆盖原文件
+打开脚本所在目录的命令行，运行以下命令即可
 
-`python main.py markdown.md [...]`
+```
+# get help
+python main.py -h
+python main.py --help
+# run the script
+python main.py -f markdown.md[[,m1.md,m2.md,...] -r True]
+python main.py -file=markdown.md[[,m1.md,m2.md,...] -replace=True]
+```
 
-> **markdown.md替换为要生成TOC的markdown文件路径，后面可跟多个文件同时生成**
 
-## 特别情况考虑
+- 多个文件时用,分割
+
+- 默认生成新的markdown不覆盖原文件，添加参数replace=True将覆盖原文件
+
+## 特殊情况考虑
+
+- python代码中的注释避免当做标题处理
+
+    - 通过正则表达式把代码块` ```xxx``` `都删除
 
 - 标题级别大小乱序
-
-    - 把标题分出多个递增部分来处理
-
+    
+    - 把标题分成多个部分来处理，每个部分具有这样的特征：第一个级别最高，后面的都能被递归拆成这样的部分。
+        
+        - 例如`[4,5,6,1,2,3,3,3,4,2]->[[4,5,6],[1,2,3,3,3,4,2]]`
+    
+    - 标题级别跳级的，将把跳级的标题看做上一个标题的下一级别。
+        
+        - 例如`[1,2,3,3,2,3,3,'5',4,4]`，为了能显示出来，将把5看做4
+    
 - 标题和`#`之间没有空格
 
     - 可以修剪
@@ -34,7 +63,7 @@ python脚本，用于生成markdown文件的TableOfContent，解决Github Flavor
 
     - 抛弃
     
-- 没有任何标题
+- markdown文件没有任何标题
     
     - 不生成锚点
 
--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import os
+import sys
+import getopt
+import re
+
 try:
     import chardet
 except:
@@ -20,6 +25,8 @@ def read_md(file):
 
 # 从读取的markdown获取标题级别列表level_list和标题名字列表name_list
 def get_level_name(md):
+    # 替换掉python或其他代码块中‘#’开头的注释
+    md = re.sub("```.*?```", "", md, flags=re.S)
     md_list = md.split('\n')
     title_list = [string for string in md_list if string.startswith('#')]
 
@@ -35,54 +42,83 @@ def get_level_name(md):
 
 # 从level_list和name_list获取标题的锚点列表result_list
 def get_result(level_list, name_list):
-    # 把level_list分割为递增的几部分，用'#'做分割标记
-    # 例如[1, 2, 3, 4, 4, 4, 3, 3, 2, 4, 5] -> [1, 2, 3, 4, 4, 4, '#', 3, 3, '#', 2, 4, 5]
-    temp = level_list[0]
-    i = 0
-    for x in level_list[1:]:
-        if x < temp:
-            i = i + 1
-            level_list.insert(i, '#')
-        temp = x
-        i = i + 1
-    # 根据level_list获取每个标题的锚点result_list
-    result_list = []
-    name_index = 0
-    tab = 0 # 制表符的数目
-    for i, x in enumerate(level_list):
-        if x != '#':
-            tab_string = "\t" * tab
-            result = tab_string + "- [" + name_list[name_index] + "](#" + name_list[name_index] + ")"
-            result_list.append(result)
-            if i + 1 != len(level_list) and level_list[i] != level_list[i + 1]:
-                tab = tab + 1
-            name_index = name_index + 1
-        else:
-            tab = 0
-            continue
-    return result_list
+    if len(level_list) > 0:
+        level_list_new = []
+        top = level_list[0]
+        temp_list = [top]
+        for x in level_list[1:] :
+            if x > top :
+                temp_list.append(x)
+            else :
+                top = x
+                level_list_new.append(temp_list)
+                temp_list = [top]
+        level_list_new.append(temp_list)
+
+        result_list = []
+        index = 0
+        for temp_list in level_list_new :
+            top = temp_list[0]
+            last_tab_count = -1
+            for x in temp_list :
+                tab_count = x - top
+                if tab_count - last_tab_count > 1:
+                    tab_count = last_tab_count + 1
+                last_tab_count = tab_count
+                tabs = "\t" * tab_count
+                result = tabs + "- [" + name_list[index] + "](#" + name_list[index] + ")"
+                result_list.append(result)
+                index += 1
+    return '\n\n'.join(result_list) + '\n\n'
 
 # 将新的markdown文件写入到原来的markdown文件
-def write_to_file(result_list, file):
-    result = '\n\n'.join(result_list) + '\n\n'
+def write_to_file(result, file,):
     md = read_md(file)
-
     encoding = get_encoding(file)
     md_new = open(file, 'w', encoding=encoding)
     md_new.write(result + md)
     md_new.close()
-    return result
+
+# 解析参数
+def get_args(argv):
+    files = []
+    replace = False
+    try :
+        # h表示帮助，f表示要转换的文件，r表示是否替换原文件
+        opts, args = getopt.getopt(argv, "hf:r:", ["help", "file=", "replace="])
+    except getopt.GetoptError :
+        print("Error: main.py -f <file> -r <replace>")
+        print("   or: main.py --file=<file> -replace=<replace>")
+        sys.exit()
+
+    for opt, arg in opts :
+        if opt in ("-h", "--help") :
+            print("main.py -f <file> -r <replace>")
+            print("or: main.py --file=<file> -replace=<replace>")
+            sys.exit()
+        elif opt in ("-f", "--file") :
+            arg = re.sub("\s", "", arg)
+            files = re.split("[,，]", arg)
+        elif opt in ("-r", "--replace") :
+            if arg == "True":
+                replace = True
+
+    return files, replace
 
 
 if __name__ == "__main__":
-    import sys
-    files = sys.argv[1:]
+    argv = sys.argv[1:]
+    files, replace = get_args(argv)
     for file in files:
-        md = read_md(file)
-        level_list, name_list = get_level_name(md)
-        if len(name_list) > 0 :
-            result_list = get_result(level_list, name_list)
-            result =  write_to_file(result_list, file)
-            print(file, '\n\n', result)
+        if os.path.exists(file) :
+            md = read_md(file)
+            level_list, name_list = get_level_name(md)
+            if len(name_list) > 0 :
+                result= get_result(level_list, name_list)
+                if replace :
+                    write_to_file(result, file)
+                print(file, '\n\n', result)
+            else :
+                print(file, '\n\n', "No Title")
         else :
-            print(file, '\n\n', "No title")
+            print("Error: %s is not exist" % file)
